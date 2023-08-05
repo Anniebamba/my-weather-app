e0f2b501989df20022c8ef962e6e452dfa585100
Anniebamba <135807502+Anniebamba@users.noreply.github.com>
2023-08-05 17:13:42 +0100

    updated the date section

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hour = date.getHours();
+  if (hour < 10) {
+    `0${hour}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hour}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   console.log(response.data.main.temp);
@@ -9,9 +32,10 @@ function displayTemperature(response) {
   humidityElement.innerHTML = `Humidity: ${response.data.main.humidity}%`;
   let windSpeedElement = document.querySelector("#wind-speed");
   windSpeedElement.innerHTML = `Wind Speed: ${response.data.wind.speed}Km/h`;
-
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "466e9221886a6c1c8d5c5e06eee0ec31";
