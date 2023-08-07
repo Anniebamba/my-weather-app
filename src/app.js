a9af72381be8963ebca26e962134764fa1cc046d
Anniebamba <135807502+Anniebamba@users.noreply.github.com>
2023-08-07 01:26:49 +0100

    added unit conversion

--- src/app.js
+++ src/app.js
@@ -35,7 +35,8 @@ function displayTemperature(response) {
     response.data.wind.speed
   )}Km/h`;
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+   celciusTemperature = Math.round(response.data.main.temp);
+  temperatureElement.innerHTML = celciusTemperature;
   let dateElement = document.querySelector("#date");
   dateElement.innerHTML = `Last updated ${formatDate(response.data.dt * 1000)}`;
   let iconElement = document.querySelector("#icon");
@@ -44,19 +45,42 @@ function displayTemperature(response) {
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+ 
 }
+
 function search(city) {
   let apiKey = "466e9221886a6c1c8d5c5e06eee0ec31";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}
 &appid=${apiKey}&units=metric`;
-
-  axios.get(apiUrl).then(displayTemperature);
+axios.get(apiUrl).then(displayTemperature);
 }
 function workOnSearch(event) {
   event.preventDefault();
   let cityInputElement = document.querySelector("#city-input");
   search(cityInputElement.value);
 }
+function farenheitTemp(event){
+  event.preventDefault();
+  celciusLink.classList.remove("default");
+  farenheitLink.classList.add("default");
+ let farenheitTemperature = celciusTemperature * (9 / 5) + 32;
+ let temperatureElement=document.querySelector("#temperature");
+ temperatureElement.innerHTML= Math.round(farenheitTemperature)
+}
 
+function celciusTemp(event){
+  event.preventDefault();
+  celciusLink.classList.add("default");
+  farenheitLink.classList.remove("default");
+  temperatureElement=document.querySelector("#temperature");
+  temperatureElement.innerHTML=celciusTemperature;
+}
 let form = document.querySelector("#search");
 form.addEventListener("submit", workOnSearch);
+
+let farenheitLink=document.querySelector("#farenheit");
+farenheitLink.addEventListener("click", farenheitTemp);
+
+let celciusTemperature= null;
+let celciusLink= document.querySelector("#celcius");
+celciusLink.addEventListener("click", celciusTemp);
