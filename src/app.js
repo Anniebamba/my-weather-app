4fbc5982b72f6fe4b13a047bb7133f24fd0a0ae5
Anniebamba <135807502+Anniebamba@users.noreply.github.com>
2023-08-11 14:08:29 +0100

    moved forecast div code to js
    looped the forecast function to create multiple forecast days.

--- src/app.js
+++ src/app.js
@@ -2,11 +2,11 @@ function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hour = date.getHours();
   if (hour < 10) {
-    `0${hour}`;
+    hour = `0${hour}`;
   }
   let minutes = date.getMinutes();
   if (minutes < 10) {
-    `0${minutes}`;
+    minutes = `0${minutes}`;
   }
   let days = [
     "Sunday",
@@ -20,6 +20,31 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day} ${hour}:${minutes}`;
 }
+
+function displayForecast(){
+  forecastElement= document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Sat", "Sun","Mon","Tue","Wed","Thur"]
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+                <div class="col-2">
+                <div class="weather-forecast-date">
+                  ${day}</div>
+                  <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" width="35">
+                  <div class="weather-forecast-temp">
+                  <span class="weather-forecast-temp-min">18°</span><span class="weather-forecast-temp-max">/20°</span>
+                </div>
+                </div>`;
+  });
+    forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+  ;
+}
+
+
 
 function displayTemperature(response) {
   console.log(response.data);
@@ -35,7 +60,7 @@ function displayTemperature(response) {
     response.data.wind.speed
   )}Km/h`;
   let temperatureElement = document.querySelector("#temperature");
-   celciusTemperature = Math.round(response.data.main.temp);
+  celciusTemperature = Math.round(response.data.main.temp);
   temperatureElement.innerHTML = celciusTemperature;
   let dateElement = document.querySelector("#date");
   dateElement.innerHTML = `Last updated ${formatDate(response.data.dt * 1000)}`;
@@ -45,42 +70,42 @@ function displayTemperature(response) {
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
- 
 }
 
 function search(city) {
   let apiKey = "466e9221886a6c1c8d5c5e06eee0ec31";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}
 &appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayTemperature);
+  axios.get(apiUrl).then(displayTemperature);
 }
 function workOnSearch(event) {
   event.preventDefault();
   let cityInputElement = document.querySelector("#city-input");
   search(cityInputElement.value);
 }
-function farenheitTemp(event){
+function farenheitTemp(event) {
   event.preventDefault();
   celciusLink.classList.remove("default");
   farenheitLink.classList.add("default");
- let farenheitTemperature = celciusTemperature * (9 / 5) + 32;
- let temperatureElement=document.querySelector("#temperature");
- temperatureElement.innerHTML= Math.round(farenheitTemperature)
+  let farenheitTemperature = celciusTemperature * (9 / 5) + 32;
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(farenheitTemperature);
 }
 
-function celciusTemp(event){
+function celciusTemp(event) {
   event.preventDefault();
   celciusLink.classList.add("default");
   farenheitLink.classList.remove("default");
-  temperatureElement=document.querySelector("#temperature");
-  temperatureElement.innerHTML=celciusTemperature;
+  temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = celciusTemperature;
 }
 let form = document.querySelector("#search");
 form.addEventListener("submit", workOnSearch);
 
-let farenheitLink=document.querySelector("#farenheit");
+let farenheitLink = document.querySelector("#farenheit");
 farenheitLink.addEventListener("click", farenheitTemp);
 
-let celciusTemperature= null;
-let celciusLink= document.querySelector("#celcius");
+let celciusTemperature = null;
+let celciusLink = document.querySelector("#celcius");
 celciusLink.addEventListener("click", celciusTemp);
+displayForecast();
